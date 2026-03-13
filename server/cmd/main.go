3fb760c0186171e60cfa9d61eb6c8706eaeb4019
zed07 <houssine.ikhlef@proton.me>
2026-03-13 15:04:38 -0400

    Implement sql operations and handlers

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -12,6 +12,8 @@ import (
 	"github.com/function09/order_management_system/server/config"
 	"github.com/function09/order_management_system/server/db"
 	"github.com/function09/order_management_system/server/internal/auth"
+	"github.com/function09/order_management_system/server/internal/products"
+	"github.com/function09/order_management_system/server/middleware"
 	"github.com/joho/godotenv"
 )
 
@@ -32,11 +34,17 @@ func main() {
 		Addr:    cfg.Port,
 	}
 
-	store := &auth.Store{DB: database}
+	authStore := &auth.Store{DB: database}
+	productsStore := &products.Store{DB: database}
 
-	mux.HandleFunc("POST /auth/register", auth.RegisterUserHandler(store))
-	mux.HandleFunc("POST /auth/login", auth.LoginUserHandler(store, cfg.JWTSecret))
+	mux.HandleFunc("POST /auth/register", auth.RegisterUserHandler(authStore))
+	mux.HandleFunc("POST /auth/login", auth.LoginUserHandler(authStore, cfg.JWTSecret))
 	mux.HandleFunc("POST /auth/logout", auth.LogOutHandler)
+	mux.HandleFunc("GET /products", middleware.AuthMiddleware(cfg.JWTSecret, products.GetAllProductsHandler(productsStore)))
+	mux.HandleFunc("GET /products/{id}", middleware.AuthMiddleware(cfg.JWTSecret, products.GetProductHandler(productsStore)))
+	mux.HandleFunc("POST /products", middleware.AuthMiddleware(cfg.JWTSecret, products.AddProductHandler(productsStore)))
+	mux.HandleFunc("DELETE /products/{id}", middleware.AuthMiddleware(cfg.JWTSecret, products.RemoveProductHandler(productsStore)))
+	mux.HandleFunc("PUT /products/{id}", middleware.AuthMiddleware(cfg.JWTSecret, products.UpdateProductHandler(productsStore)))
 
 	go func() {
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
--- server/internal/products/store.go
+++ server/internal/products/store.go
@@ -1,6 +1,8 @@
 package products
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
 type Product struct {
 	ID         int
@@ -20,4 +22,61 @@ type ProductStore interface {
 	AddProduct(p *Product) error
 	RemoveProduct(p *Product) error
 	UpdateProduct(p *Product) error
+}
+
+func (s *Store) GetAllProducts() ([]*Product, error) {
+	rows, err := s.Query("SELECT id, name, price, quantity, category_id FROM products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []*Product
+
+	for rows.Next() {
+		var prod Product
+
+		if err := rows.Scan(&prod.ID, &prod.Name, &prod.Price, &prod.Quantity, &prod.CategoryID); err != nil {
+			return products, err
+		}
+		products = append(products, &prod)
+	}
+
+	return products, nil
+}
+
+func (s *Store) GetProduct(id int) (*Product, error) {
+	var product Product
+	if err := s.QueryRow("SELECT id, name, price, quantity, category_id FROM products WHERE id = $1", id).Scan(&product.ID, &product.Name, &product.Price, &product.Quantity, &product.CategoryID); err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
+
+func (s *Store) AddProduct(p *Product) error {
+	_, err := s.Exec("INSERT INTO products (name, price, quantity, category_id) VALUES ($1, $2, $3, $4)", p.Name, p.Price, p.Quantity, p.CategoryID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Store) RemoveProduct(p *Product) error {
+	_, err := s.Exec("DELETE FROM products WHERE id = $1", p.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Store) UpdateProduct(p *Product) error {
+	_, err := s.Exec("UPDATE products SET name = $1, price = $2, quantity = $3, category_id = $4 WHERE id = $5", p.Name, p.Price, p.Quantity, p.CategoryID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
